5ef46cf062ae174d8be74e8c7b5a79843af946a8
Stefan Boos <kontakt@boos.systems>
2022-11-04 05:11:03 +0100

    initial project created with lein new figwheel-main game-of-life

---
+++ test/game_of_life/core_test.cljs
@@ -0,0 +1,10 @@
+(ns game-of-life.core-test
+    (:require
+     [cljs.test :refer-macros [deftest is testing]]
+     [game-of-life.core :refer [multiply]]))
+
+(deftest multiply-test
+  (is (= (* 1 2) (multiply 1 2))))
+
+(deftest multiply-test-2
+  (is (= (* 75 10) (multiply 10 75))))
